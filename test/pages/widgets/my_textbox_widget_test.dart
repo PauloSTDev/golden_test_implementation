ee4caeec09deacdef95700929417d46f1d0a5b92
Paulo Santos <paulo.santos@compasso.com.br>
2024-11-19 18:59:53 -0300

    Adiciona testes de MyTextboxWidget

--- test/pages/widgets/my_textbox_widget_test.dart
+++ test/pages/widgets/my_textbox_widget_test.dart
@@ -1,7 +1,31 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_test_implementation/pages/widgets/my_textbox_widget.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
-  test('MyTextboxWidget', () {
-    // Test code goes here.
+  testGoldens('MyTextboxWidget - Password Input', (tester) async {
+    //arrange
+    await loadAppFonts();
+    final builder = GoldenBuilder.grid(
+      columns: 2,
+      widthToHeightRatio: 1,
+    )
+      ..addScenario(
+        "Password Empty",
+        const MyTextboxWidget(),
+      )
+      ..addScenario(
+        "Password Hided",
+        const MyTextboxWidget(obscureText: true, initialValue: 'Teste'),
+      )
+      ..addScenario(
+        "Password Showed",
+        const MyTextboxWidget(obscureText: false, initialValue: 'Teste'),
+      );
+
+    //act
+    await tester.pumpWidgetBuilder(builder.build());
+
+    //assert
+    await screenMatchesGolden(tester, 'my_textbox_widget_password_input_test');
   });
 }

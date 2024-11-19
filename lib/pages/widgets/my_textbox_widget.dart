6add99d3fbd1577a76818c5a34d8cd2cbaa0562f
Paulo Santos <paulo.santos@compasso.com.br>
2024-11-19 18:53:11 -0300

    Adiciona initialValue

--- lib/pages/widgets/my_textbox_widget.dart
+++ lib/pages/widgets/my_textbox_widget.dart
@@ -5,16 +5,18 @@ class MyTextboxWidget extends StatelessWidget {
     super.key,
     this.eyeOnTap,
     this.obscureText = true,
+    this.initialValue = '',
   });
 
   final VoidCallback? eyeOnTap;
   final bool obscureText;
+  final String initialValue;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       key: key,
-      initialValue: '',
+      initialValue: initialValue,
       obscureText: obscureText,
       decoration: InputDecoration(
         fillColor: Colors.yellow,

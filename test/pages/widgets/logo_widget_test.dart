e4865513ebcc1345465101d0e8bef1ad2d79c579
Paulo Santos <paulo.santos@compasso.com.br>
2024-11-20 10:09:21 -0300

    Adiciona Alchemist Golden Tests

--- test/pages/widgets/logo_widget_test.dart
+++ test/pages/widgets/logo_widget_test.dart
@@ -1,3 +1,6 @@
+import 'package:alchemist/alchemist.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_test_implementation/pages/widgets/logo_widget.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
@@ -21,5 +24,53 @@ void main() {
 
     // assert
     await screenMatchesGolden(tester, 'logo_widget');
+  });
+
+  // Alchemist Golden Test
+  group('ListTile Golden Tests', () {
+    goldenTest(
+      'renders correctly',
+      fileName: 'list_tile',
+      builder: () => GoldenTestGroup(
+        scenarioConstraints: const BoxConstraints(maxWidth: 600),
+        children: [
+          GoldenTestScenario(
+            name: 'with title',
+            child: const ListTile(title: Text('ListTile.title')),
+          ),
+          GoldenTestScenario(
+            name: 'with title and subtitle',
+            child: const ListTile(
+              title: Text('ListTile.title'),
+              subtitle: Text('ListTile.subtitle'),
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'with trailing icon',
+            child: const ListTile(
+              title: Text('ListTile.title'),
+              trailing: Icon(Icons.chevron_right_rounded),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    goldenTest(
+      'ElevatedButton renders tap indicator when pressed',
+      fileName: 'elevated_button_pressed',
+      whilePerforming: press(find.byType(ElevatedButton)),
+      builder: () => GoldenTestGroup(
+        children: [
+          GoldenTestScenario(
+            name: 'pressed',
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('Pressed'),
+            ),
+          ),
+        ],
+      ),
+    );
   });
 }
